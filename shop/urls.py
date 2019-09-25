6ce876d497afa0a0b8c0baa86c4b715285902116
Daniel Collins <dpc7ns@virginia.edu>
2019-09-24 22:12:21 -0400

    Changing the References from polls to shop

---
+++ shop/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'shop'
+urlpatterns = [
+    path('', views.LandingView, name='landing'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('suggestion/list/', views.suggestionList, name='suggestion'),
+    # path('suggestion/<int:pk>/', views.suggestionDetail, name='suggestionDetail'),
+    # path('suggestion/', views.suggestionForm, name='suggestionForm')
+]
